30fc193ebe6c332acc358a2f0e929106dee459b8
jinseop <jjs0110zz@naver.com>
2025-08-05 16:22:59 +0900

    [refactor] script scenario1 class rename 및 test파일 경로이동합니다.

--- tests/test_script_FullWrite_ReadCompare.py
+++ tests/test_script_FullWrite_ReadCompare.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import Mock
-from scripts.FullWriteReadCompare import Script1_FullWrite
+from scripts.FullWriteReadCompare import FullWriteReadCompare
 from shell.shell import Shell
 
 @pytest.fixture
@@ -9,14 +9,14 @@ def mock_shell_interface():
     return mock
 
 
-def test_script1_init(mock_shell_interface):
-    script = Script1_FullWrite(mock_shell_interface)
+def test_script_init(mock_shell_interface):
+    script = FullWriteReadCompare(mock_shell_interface)
     assert script is not None
     assert script.shell is mock_shell_interface
 
 
-def test_script1_read_0(mock_shell_interface):
-    script = Script1_FullWrite(mock_shell_interface)
+def test_script_read_0(mock_shell_interface):
+    script = FullWriteReadCompare(mock_shell_interface)
     script.run()
 
     mock_shell_interface.read.assert_called_once_with(0)
