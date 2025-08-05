9d9e72e6185456cfc43852ab37425a5f75b1d657
si <psiluv@gmail.com>
2025-08-05 16:30:59 +0900

    [add]read address boundary check

--- tests/test_controller_read.py
+++ tests/test_controller_read.py
@@ -9,6 +9,7 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 SSD_NAND_PATH = os.path.join(PROJECT_ROOT, 'ssd_nand.txt')
 SSD_OUTPUT_PATH = os.path.join(PROJECT_ROOT, 'ssd_output.txt')
 SINGLE_TEST_DATA = {1:"0xaaaaaaaa"}
+ERROR = 'ERROR'
 
 @pytest.fixture
 def controller_with_single_data():
@@ -17,13 +18,19 @@ def controller_with_single_data():
         json.dump(SINGLE_TEST_DATA, f, ensure_ascii=False)
     return controller
 
-def test_read(controller_with_single_data):
-    controller_with_single_data.read(1)
-    assert get_ssd_output() == SINGLE_TEST_DATA.get(1)
-
-
 def get_ssd_output():
     with open(SSD_OUTPUT_PATH, "r", encoding="utf-8") as f:
         actual = f.read()
     return actual
 
+def test_read(controller_with_single_data):
+    controller_with_single_data.read(1)
+    assert get_ssd_output() == SINGLE_TEST_DATA.get(1)
+
+def test_read_invalid_minus_lba(controller_with_single_data):
+    controller_with_single_data.read(-1)
+    assert get_ssd_output() == ERROR
+
+def test_read_invalid_hundred_lba(controller_with_single_data):
+    controller_with_single_data.read(100)
+    assert get_ssd_output() == ERROR
