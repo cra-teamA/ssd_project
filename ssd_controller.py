c21baea02fe9124d6962e273c1192b5e88e3163e
si <psiluv@gmail.com>
2025-08-05 16:25:36 +0900

    [refactor]add read

--- ssd_controller.py
+++ ssd_controller.py
@@ -1,15 +1,13 @@
 import json
 SSD_NAND_PATH = 'ssd_nand.txt'
-SSD_OUT_PATH = 'ssd_output.txt'
+SSD_OUTPUT_PATH = 'ssd_output.txt'
 class SSDController:
     def __init__(self):
         ...
     def read(self,addr:int):
-
         with open(SSD_NAND_PATH, "r", encoding="utf-8") as f:
             data = json.load(f).get(str(addr))
-        print(data)
-        with open(SSD_OUT_PATH, "w", encoding="utf-8") as f:
+        with open(SSD_OUTPUT_PATH, "w", encoding="utf-8") as f:
             f.write(data)
 
     def write(self,addr, value):
--- tests/test_controller_read.py
+++ tests/test_controller_read.py
@@ -1,3 +1,5 @@
+import pytest
+
 from ssd_controller import SSDController
 import json
 import os
@@ -5,14 +7,23 @@ import os
 # 현재 테스트 파일 위치 기준으로 루트 디렉토리 계산
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 SSD_NAND_PATH = os.path.join(PROJECT_ROOT, 'ssd_nand.txt')
-SSD_OUT_PATH = os.path.join(PROJECT_ROOT, 'ssd_output.txt')
+SSD_OUTPUT_PATH = os.path.join(PROJECT_ROOT, 'ssd_output.txt')
 SINGLE_TEST_DATA = {1:"0xaaaaaaaa"}
-def test_read():
-    print(PROJECT_ROOT)
+
+@pytest.fixture
+def controller_with_single_data():
     controller = SSDController()
     with open(SSD_NAND_PATH, "w", encoding="utf-8") as f:
         json.dump(SINGLE_TEST_DATA, f, ensure_ascii=False)
-    controller.read(1)
-    with open(SSD_OUT_PATH, "r", encoding="utf-8") as f:
+    return controller
+
+def test_read(controller_with_single_data):
+    controller_with_single_data.read(1)
+    assert get_ssd_output() == SINGLE_TEST_DATA.get(1)
+
+
+def get_ssd_output():
+    with open(SSD_OUTPUT_PATH, "r", encoding="utf-8") as f:
         actual = f.read()
-    assert actual == SINGLE_TEST_DATA.get(1)
+    return actual
+
