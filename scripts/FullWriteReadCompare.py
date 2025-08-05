21135fab789323a60634f340de364482db1cc7da
jinseop <jjs0110zz@naver.com>
2025-08-05 16:03:43 +0900

    [feature]script scenario1 skeleton code 작성

---
+++ scripts/FullWriteReadCompare.py
@@ -0,0 +1,10 @@
+from scripts.BaseScript import BaseScript
+from shell.shell import Shell
+
+class Script1_FullWrite(BaseScript):
+    def __init__(self, shell_interface:Shell):
+        super().__init__(shell_interface)
+
+
+    def run(self):
+        pass
