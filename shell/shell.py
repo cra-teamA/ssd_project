833181d1d2a0806a08d1b06adccfd2fdff63df63
hwieun <hwieunlee98@gmail.com>
2025-08-05 16:15:51 +0900

    [refactor] refactor test_shell_read

--- shell/shell.py
+++ shell/shell.py
@@ -5,14 +5,21 @@ class Shell:
     def __init__(self):
         ...
 
-    def read(self, command_lba):
-        lba = 3
+    def read(self, read_command):
+        lba = self.get_lba_from_read_command(read_command)
         subprocess.run(
-            ["python", "ssd.py", "R", str(lba)],
+            ["python", "ssd.py", "R", lba],
             capture_output=True,
             text=True
         )
         output = 'ssd_output.txt'
         with open(output, 'r', encoding='utf-8') as file:
             line = file.readline().strip()
-        print(f"[Read] LBA {lba} : {line}")
+        if line == "ERROR":
+            print("[Read] ERROR")
+        else:
+            print(f"[Read] LBA {lba} : {line}")
+
+    def get_lba_from_read_command(self, read_command: str) -> str:
+        lba = read_command.split()[1]
+        return lba
