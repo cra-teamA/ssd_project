a389e0e5367713cfe88facc8410c6cee114b6358
hwieun <hwieunlee98@gmail.com>
2025-08-05 16:39:51 +0900

    [feature] add test_shell_help

--- shell/shell.py
+++ shell/shell.py
@@ -20,6 +20,21 @@ class Shell:
         else:
             print(f"[Read] LBA {lba} : {line}")
 
+    def help(self):
+        message = '''
+        제작자: [Team All Clear] 장진섭 팀장, 박성일, 이규홍, 최준식, 임소현, 이휘은
+        명령어 사용 법 :
+         1. read: read [LBA]
+         2. write: write [LBA] [VALUE]
+         3. fullwrite: fullwrite [VALUE]
+         4. fullread: fullread
+         5. 1_FullWriteAndReadCompare: 1_ 혹은 1_FullWriteAndReadCompare 입력
+         6. 2_PartialLBAWrite: 2_ 혹은 2_PartialLBAWrite 입력
+         7. 3_WriteReadAging: 3_ 혹은 3_WriteReadAging 입력
+         8. exit: exit
+        그 외 명령어 입력 시, INVALID COMMAND 가 출력 됩니다.'''
+        print(message)
+
     def get_lba_from_read_command(self, read_command: str) -> str:
         lba = read_command.split()[1]
         return lba
