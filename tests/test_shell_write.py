d9c7c02d757f34454d00b07d907a1b76c1bfd429
soiscow <thgus_so@naver.com>
2025-08-05 16:06:48 +0900

    [refactor] add shell_and_subprocess_mocker fixture

--- tests/test_shell_write.py
+++ tests/test_shell_write.py
@@ -2,9 +2,15 @@ import pytest
 from shell.shell import Shell
 
 
-def test_shell_write(capsys, mocker):
+@pytest.fixture
+def shell_and_subprocess_mocker(mocker):
     shell = Shell()
     mock_run = mocker.patch("shell.shell.subprocess.run")
+    return shell, mock_run
+
+
+def test_shell_write(capsys, shell_and_subprocess_mocker):
+    shell, mock_run = shell_and_subprocess_mocker
     shell.write("write 3 0xAAAABBBB")
 
     captured = capsys.readouterr()
@@ -12,12 +18,10 @@ def test_shell_write(capsys, mocker):
     mock_run.assert_called_once_with(["ssd", "W", "3", "0xAAAABBBB"])
 
 
-def test_shell_write_valid_check_address(capsys, mocker):
-    shell = Shell()
-    mock_run = mocker.patch("shell.shell.subprocess.run")
+def test_shell_write_valid_check_address(capsys, shell_and_subprocess_mocker):
+    shell, mock_run = shell_and_subprocess_mocker
     shell.write("write 1000 0xAAAABBBB")
-    
+
     captured = capsys.readouterr()
     assert captured.out == "INVALID COMMAND\n"
     assert mock_run.call_count == 0
-
