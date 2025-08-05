80e186e62dc8225bb8643b24328b0f067428385f
junsik <junsik@ma.il>
2025-08-05 17:24:17 +0900

    [refactor] write is_invalid_input 메서드 추출

--- ssd_controller.py
+++ ssd_controller.py
@@ -1,15 +1,22 @@
 class SSDController:
     def write(self, addr: int, val: str) -> bool:
-        if not isinstance(addr, int):
+
+        if self.is_invalid_input(addr, val):
             return False
-        if addr < 0 or addr > 99:
-            return False
-        if not isinstance(val, str):
-            return False
-        if len(val) > 10:
-            return False
-        if not (val.startswith('0x') or val.startswith('0X') ):
-            return False
-        if not set(val[2:]).issubset(set("0123456789abcdef")):
-            return False
+
         return True
+
+    def is_invalid_input(self, addr, val):
+        if not isinstance(addr, int):
+            return True
+        if addr < 0 or addr > 99:
+            return True
+        if not isinstance(val, str):
+            return True
+        if len(val) > 10:
+            return True
+        if not (val.startswith('0x') or val.startswith('0X')):
+            return True
+        if not set(val[2:]).issubset(set("0123456789abcdef")):
+            return True
+        return False
