b7927717966ca6ecec1e7330babefba2305d1b1b
jinseop <jjs0110zz@naver.com>
2025-08-05 16:59:02 +0900

    [feature] script scenario1 write 0 test case추가

--- scripts/FullWriteReadCompare.py
+++ scripts/FullWriteReadCompare.py
@@ -7,25 +7,6 @@ class FullWriteReadCompare(BaseScript):
 
 
     def run(self):
-        lba_total = 100
-        group_size = 5
-        group_count = lba_total // group_size
-        try:
-            for group in range(group_count):
-                value = f"0x{group:08x}"
-
-                start_lba = group * group_size
-                end_lba = start_lba + group_size
-
-                for lba in range(start_lba, end_lba):
-                    self.shell.write(f"W {lba} {value}")
-
-                for lba in range(start_lba, end_lba):
-                    result = self.shell.read(f"R {lba}")
-                    if result != value:
-                        return False
-
-            return True
-        except Exception as e:
-            print(f"Exception during write/read: {e}")
-            return False
+        self.shell.read(0)
+        self.shell.write(1,"0x00000000")
+        pass
