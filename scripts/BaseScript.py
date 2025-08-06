6bf585562ba95e56bf5fb39374dc8c56e07faabf
gyuhong <jagijudo@gmail.com>
2025-08-06 09:39:23 +0900

    BaseScript에 공통함수 추가

--- scripts/BaseScript.py
+++ scripts/BaseScript.py
@@ -4,6 +4,12 @@ class BaseScript(ABC):
     def __init__(self, shell_interface):
         self.shell = shell_interface  # read/write interface
 
+    def write_lba(self, lba, data):
+        self.shell.write(lba, data)
+
+    def read_lba(self, lba):
+        return self.shell.read(lba)
+
     @abstractmethod
     def run(self):
         pass
