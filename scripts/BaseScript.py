7d79705cc95bf53e6d4dc7d08d1af113ae0e6428
jinseop <jjs0110zz@naver.com>
2025-08-06 13:10:20 +0900

    [feature] script scenario1 shell sehll interface 직접 호출에서 BaseScript 사용하도록 수정

--- scripts/BaseScript.py
+++ scripts/BaseScript.py
@@ -1,14 +1,18 @@
 from abc import ABC, abstractmethod
+from shell.shell import Shell
+
 
 class BaseScript(ABC):
-    def __init__(self, shell_interface):
+    def __init__(self, shell_interface : Shell):
         self.shell = shell_interface  # read/write interface
 
-    def write_lba(self, lba, data):
-        self.shell.write(lba, data)
+    def write_lba(self, lba : str, data : str):
+        cmd = f"write {lba} {data}"
+        self.shell.write(cmd)
 
-    def read_lba(self, lba):
-        return self.shell.read(lba)
+    def read_lba(self, lba: str):
+        cmd = f"read {lba}"
+        return self.shell.read(cmd)
 
     @abstractmethod
     def run(self):
