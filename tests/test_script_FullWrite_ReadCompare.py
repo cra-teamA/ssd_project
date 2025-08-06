d248230c2320704e42c47c921ad9469b39c9aa81
jinseop <jjs0110zz@naver.com>
2025-08-06 09:43:44 +0900

    [feature] script scenario1 fail test case 추가

--- tests/test_script_FullWrite_ReadCompare.py
+++ tests/test_script_FullWrite_ReadCompare.py
@@ -10,13 +10,25 @@ def mock_shell_interface():
 
 
 @pytest.fixture
-def mock__read_shell_interface():
+def mock_shell_interface_read_pass():
     mock = Mock()
     read_side_effect = []
     #read_side_effect = 0,0,0,0,0,1,1,1,1,1, ... ,19,19,19,19,19
     for group in range(20):
         value = f"0x{group:08x}"
         read_side_effect.extend([value] * 5)
+    mock.read.side_effect = read_side_effect
+    return mock
+
+@pytest.fixture
+def mock_shell_interface_read_fail():
+    mock = Mock()
+    read_side_effect = []
+    #read_side_effect = 0,0,0,0,0,1,1,1,1,1, ... ,19,19,19,19,19
+    for group in range(20):
+        value = f"0x{group:08x}"
+        read_side_effect.extend([value] * 5)
+    read_side_effect[12]=f"0x{7:08x}"
     mock.read.side_effect = read_side_effect
     return mock
 
@@ -37,7 +49,7 @@ def test_script_write_0_0x00000000(mock_shell_interface):
     script.run()
     mock_shell_interface.write.assert_any_call("W 1 0x00000000")
 
-def test_script_write_all_read_compare(mock__read_shell_interface):
+def test_script_write_all_read_compare_pass(mock__read_shell_interface):
     script = FullWriteReadCompare(mock__read_shell_interface)
     script.run()
     assert mock__read_shell_interface.write.call_count == 100
@@ -50,3 +62,9 @@ def test_script_write_all_read_compare(mock__read_shell_interface):
         expected_read = f"R {i}"
         assert w_call.args[0] == expected_write
         assert r_call.args[0] == expected_read
+
+def test_script_write_all_read_compare_fail(mock_shell_interface_read_fail):
+    script = FullWriteReadCompare(mock_shell_interface_read_fail)
+    script.run()
+    assert mock_shell_interface_read_fail.write.call_count == 15
+    assert mock_shell_interface_read_fail.read.call_count == 13
