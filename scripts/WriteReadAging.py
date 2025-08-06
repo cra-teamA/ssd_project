640138ee76f7ea363d35d5e94e61b66e4c3c25ce
gyuhong <jagijudo@gmail.com>
2025-08-06 10:13:28 +0900

    WriteReadAging 코드 및 Test 코드 추가

--- scripts/WriteReadAging.py
+++ scripts/WriteReadAging.py
@@ -6,4 +6,15 @@ class WriteReadAging(BaseScript):
         super().__init__(shell_interface)
 
     def run(self):
-        pass
+        LOOP_COUNT = 100
+
+        for _ in range(LOOP_COUNT):
+            data1 = random.randint(1, 0x7fffffff)
+            data2 = random.randint(1, 0x7fffffff)
+
+            self.write_lba(0, data1)
+            self.write_lba(99, data2)
+
+            if data1 != self.read_lba(0) or data2 != self.read_lba(99):
+                return False
+        return True
