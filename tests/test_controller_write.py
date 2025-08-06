11d064ea2ad35bf89e26124f734753cf49e65908
junsik <junsik@ma.il>
2025-08-06 11:51:10 +0900

    [feature] test_create_ssd_controller_instance 추가

--- tests/test_controller_write.py
+++ tests/test_controller_write.py
@@ -1,1 +1,6 @@
-#작성시작 1st 커밋용
+import pytest
+from ssd_controller import SSDController
+
+def test_create_ssd_controller_instance():
+    ssd = SSDController()
+    assert isinstance(ssd, SSDController)
