2286e266add33392d88050e6ef2d876cc54f2a5a
jinseop <jjs0110zz@naver.com>
2025-08-06 09:24:09 +0900

    [feature] script scenario1 test효율화를 위해 random함수 제거 및 readCompare return value 추가

--- tests/test_script_FullWrite_ReadCompare.py
+++ tests/test_script_FullWrite_ReadCompare.py
@@ -9,6 +9,17 @@ def mock_shell_interface():
     return mock
 
 
+@pytest.fixture
+def mock__read_shell_interface():
+    mock = Mock()
+    read_side_effect = []
+    #read_side_effect = 0,0,0,0,0,1,1,1,1,1, ... ,19,19,19,19,19
+    for group in range(20):
+        value = f"0x{group:08x}"
+        read_side_effect.extend([value] * 5)
+    mock.read.side_effect = read_side_effect
+    return mock
+
 def test_script_init(mock_shell_interface):
     script = FullWriteReadCompare(mock_shell_interface)
     assert script is not None
@@ -26,8 +37,16 @@ def test_script_write_0_0x00000000(mock_shell_interface):
     script.run()
     mock_shell_interface.write.assert_any_call("W 1 0x00000000")
 
-def test_script_write_all_read_compare(mock_shell_interface):
-    script = FullWriteReadCompare(mock_shell_interface)
+def test_script_write_all_read_compare(mock__read_shell_interface):
+    script = FullWriteReadCompare(mock__read_shell_interface)
     script.run()
-    assert mock_shell_interface.write.call_count == 100
-    assert mock_shell_interface.read.call_count == 100
+    assert mock__read_shell_interface.write.call_count == 100
+    assert mock__read_shell_interface.read.call_count == 100
+
+    for i, (w_call, r_call) in enumerate(zip(mock__read_shell_interface.write.call_args_list, mock__read_shell_interface.read.call_args_list)):
+        group = i // 5
+        expected_value = f"0x{group:08x}"
+        expected_write = f"W {i} {expected_value}"
+        expected_read = f"R {i}"
+        assert w_call.args[0] == expected_write
+        assert r_call.args[0] == expected_read
