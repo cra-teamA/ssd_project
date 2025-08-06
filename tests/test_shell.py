35825f9d7dad2437020e3a8a4629e8b2b35c0d06
hwieun <hwieunlee98@gmail.com>
2025-08-06 14:47:13 +0900

    [refactor] import 파일 정리 및 MockerFixture typehint 추가

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -2,34 +2,37 @@ from unittest.mock import mock_open
 import pytest
 from pytest_mock import mocker, MockerFixture
 from shell.shell import Shell, is_valid_command
+from unittest.mock import call
+
+@pytest.fixture
+def shell_and_subprocess_mocker(mocker: MockerFixture):
+    shell = Shell()
+    mock_run = mocker.patch("shell.shell.subprocess.run")
+    return shell, mock_run
 
 
-def test_shell_command_valid(mocker):
+def test_shell_command_valid(mocker: MockerFixture):
     mocker.patch("builtins.input", return_value="write")
     ret = is_valid_command()
     assert ret is True
 
 
-def test_shell_command_invalid_foramt(mocker, capsys):
+def test_shell_command_invalid_foramt(mocker: MockerFixture, capsys):
     mocker.patch("builtins.input", return_value="hello")
-    is_valid_command()
+    ret = is_valid_command()
     output = capsys.readouterr()
     assert output.out == 'INVALID COMMAND\n'
+    assert ret is False
 
 
-
-def test_shell_exit(mocker, capsys):
+def test_shell_exit(mocker: MockerFixture, capsys):
     shell = Shell()
-    mocker.patch("builtins.input", return_value="exit")
     mocker_exit = mocker.patch("sys.exit")
     shell.exit()
     output = capsys.readouterr()
     mocker_exit.assert_called_once_with(0)
     assert output.out == "Exiting shell...\n"
 
-from unittest.mock import mock_open, patch, call
-from pytest_mock import MockerFixture
-from shell.shell import Shell
 
 def test_shell_full_read_valid(mocker: MockerFixture, capsys):
     mk_full_read = mocker.patch('shell.shell.Shell._read')
@@ -47,22 +50,17 @@ def test_shell_help(capsys):
     shell.help()
     captured = capsys.readouterr()
     assert captured.out == ('\n'
- '        제작자: [Team All Clear] 장진섭 팀장, 박성일, 이규홍, 최준식, 임소현, 이휘은\n'
- '        명령어 사용 법 :\n'
- '         1. read: read [LBA]\n'
- '         2. write: write [LBA] [VALUE]\n'
- '         3. fullwrite: fullwrite [VALUE]\n'
- '         4. fullread: fullread\n'
- '         5. 1_FullWriteAndReadCompare: 1_ 혹은 1_FullWriteAndReadCompare 입력\n'
- '         6. 2_PartialLBAWrite: 2_ 혹은 2_PartialLBAWrite 입력\n'
- '         7. 3_WriteReadAging: 3_ 혹은 3_WriteReadAging 입력\n'
- '         8. exit: exit\n'
- '        그 외 명령어 입력 시, INVALID COMMAND 가 출력 됩니다.\n')
-
-import os
-from unittest.mock import mock_open
-from pytest_mock import mocker, MockerFixture
-import pytest
+                            '        제작자: [Team All Clear] 장진섭 팀장, 박성일, 이규홍, 최준식, 임소현, 이휘은\n'
+                            '        명령어 사용 법 :\n'
+                            '         1. read: read [LBA]\n'
+                            '         2. write: write [LBA] [VALUE]\n'
+                            '         3. fullwrite: fullwrite [VALUE]\n'
+                            '         4. fullread: fullread\n'
+                            '         5. 1_FullWriteAndReadCompare: 1_ 혹은 1_FullWriteAndReadCompare 입력\n'
+                            '         6. 2_PartialLBAWrite: 2_ 혹은 2_PartialLBAWrite 입력\n'
+                            '         7. 3_WriteReadAging: 3_ 혹은 3_WriteReadAging 입력\n'
+                            '         8. exit: exit\n'
+                            '        그 외 명령어 입력 시, INVALID COMMAND 가 출력 됩니다.\n')
 
 
 def test_shell_read_valid_format(mocker: MockerFixture, capsys):
@@ -84,7 +82,7 @@ def test_shell_read_valid_format(mocker: MockerFixture, capsys):
     assert captured.out == "[Read] LBA 3 : 0xAAAABBBC\n"
 
 
-def test_shell_read_invalid_format(mocker):
+def test_shell_read_invalid_format(mocker: MockerFixture):
     mocker.patch("builtins.input", return_value="read 3")
     fake_output_content = "0xAAAABBBC\n"
     mocker.patch(
@@ -98,7 +96,7 @@ def test_shell_read_invalid_format(mocker):
         shell.read("read")
 
 
-def test_shell_valid_lba(mocker):
+def test_shell_valid_lba(mocker: MockerFixture):
     mocker.patch("builtins.input", return_value="read 3")
     mock_subprocess = mocker.patch("shell.shell.subprocess.run")
     fake_output_content = "0xAAAABBBC\n"
@@ -111,14 +109,15 @@ def test_shell_valid_lba(mocker):
     assert result == "0xAAAABBBC"
 
 
-def test_shell_invalid_lba(mocker):
+def test_shell_invalid_lba(mocker: MockerFixture):
     shell = Shell()
     with pytest.raises(ValueError):
         shell.read("read 150")  # 150은 범위 벗어남
     with pytest.raises(ValueError):
         shell.read("read abc")  # 숫자 아님
 
-def test_shell_is_script(mocker, capsys):
+
+def test_shell_is_script(mocker: MockerFixture, capsys):
     shell = Shell()
     mock_subprocess = mocker.patch("shell.shell.subprocess.run")
     fake_output_content = "0xAAAABBBC\n"
@@ -131,20 +130,6 @@ def test_shell_is_script(mocker, capsys):
 
     assert output.out == ""
     assert line == "0xAAAABBBC"
-
-import pytest
-
-from shell.shell import Shell
-from unittest.mock import call
-
-INVALID_MSG = "INVALID COMMAND\n"
-
-
-@pytest.fixture
-def shell_and_subprocess_mocker(mocker):
-    shell = Shell()
-    mock_run = mocker.patch("shell.shell.subprocess.run")
-    return shell, mock_run
 
 
 @pytest.mark.parametrize("value", ["0x0000BBBB", "0xBBBB"])
@@ -160,7 +145,7 @@ def test_shell_write_cmd_success(capsys, shell_and_subprocess_mocker, value):
 @pytest.mark.parametrize("value", ["0x0000BBBB", "0xBBBB"])
 def test_shell_write_script_success(capsys, shell_and_subprocess_mocker, value):
     shell, mock_run = shell_and_subprocess_mocker
-    shell.write(f"write 3 {value}",True)
+    shell.write(f"write 3 {value}", True)
 
     captured = capsys.readouterr()
     assert captured.out == ""
@@ -206,4 +191,3 @@ def test_shell_fullwrite_valid_check_value_fail(shell_and_subprocess_mocker, val
     with pytest.raises(ValueError):
         shell.fullwrite(f"write {value}")
     assert mock_run.call_count == 0
-
