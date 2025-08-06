ad43a21b6f5d3347fdf998849b74ec91ae2fd4cf
soiscow <thgus_so@naver.com>
2025-08-06 09:32:00 +0900

    [fix] 유실된 write 다시 추가
    rebase 과정 중
    commit id 8b3deb12 에서 사라진 write를 다시 추가하였습니다.
    
    Signed-off-by: soiscow <thgus_so@naver.com>

--- shell/shell.py
+++ shell/shell.py
@@ -42,3 +42,16 @@ class Shell:
     def get_lba_from_read_command(self, read_command: str) -> str:
         lba = read_command.split()[1]
         return lba
+
+    def write(self, read_command: str):
+        _, lba, value = read_command.split()
+        try:
+            if int(lba) < MIN_LBA or int(lba) > MAX_LBA:
+                raise ValueError
+            if len(value) > MAX_VALUE_LENGTH or not value.upper().startswith("0X"):
+                raise ValueError
+            subprocess.run(["ssd", "W", lba, f"0x{int(value, 16) :08X}"])
+            print("[Write] Done")
+        except ValueError:
+            print("INVALID COMMAND")
+            return
