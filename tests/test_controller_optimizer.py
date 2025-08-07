c401639a9e1953fbb6cfb862b0c610de58f063aa
junsik <junsik@ma.il>
2025-08-08 07:52:07 +0900

    [refactor] optimizer test case tuple -> class 활용으로 통합

--- tests/test_controller_optimizer.py
+++ tests/test_controller_optimizer.py
@@ -5,68 +5,12 @@ from core.ssd_controller import SSDController
 
 DEFAULT_VALUE = '0x00000000'
 캐시사이즈 = 100
-cases = [
-    {
-        'buff': [
-            ('E', 0, 10),
-            ('W', 5, '0x00000001')
-        ],
-        'optimized': [
-            ('E', 0, 5),
-            ('W', 5, '0x00000001'),
-            ('E', 6, 4)
-        ],
-    },
-    {
-        'buff': [
-            ('E', 0, 10),
-            ('W', 5, '0x00000001'),
-            ('W', 7, '0x00000001'),
-        ],
-        'optimized': [
-            ('E', 0, 5),
-            ('W', 5, '0x00000001'),
-            ('E', 6, 1),
-            ('W', 7, '0x00000001'),
-            ('E', 8, 2),
-        ],
-    },
-    {
-        'buff': [
-            ('E', 0, 4),
-            ('E', 4, 5)
-        ],
-        'optimized': [
-            ('E', 0, 9),
-        ],
-    },
-    {
-        'buff': [
-            ('W', 0, '0x0000000a'),
-            ('W', 0, '0x0000000b'),
-        ],
-        'optimized': [
-            ('W', 0, '0x0000000b'),
-        ],
-    },
-    {
-        'buff': [
-            ('E', 0, 3),
-            ('W', 4, '0x0000000a'),
-            ('E', 0, 5),
-
-        ],
-        'optimized': [
-            ('E', 0, 5),
-        ],
-    },
-]
 
 cases_cls = [
     {
         'buff': [
-            ('E', 0, 10),
-            ('W', 5, '0x00000001')
+            command_factory('E', 0, 10),
+            command_factory('W', 5, '0x00000001')
         ],
         'optimized': [
             command_factory('E', 0, 5),
@@ -76,9 +20,9 @@ cases_cls = [
     },
     {
         'buff': [
-            ('E', 0, 10),
-            ('W', 5, '0x00000001'),
-            ('W', 7, '0x00000001'),
+            command_factory('E', 0, 10),
+            command_factory('W', 5, '0x00000001'),
+            command_factory('W', 7, '0x00000001'),
         ],
         'optimized': [
             command_factory('E', 0, 5),
@@ -90,8 +34,8 @@ cases_cls = [
     },
     {
         'buff': [
-            ('E', 0, 4),
-            ('E', 4, 5)
+            command_factory('E', 0, 4),
+            command_factory('E', 4, 5)
         ],
         'optimized': [
             command_factory('E', 0, 9),
@@ -99,8 +43,8 @@ cases_cls = [
     },
     {
         'buff': [
-            ('W', 0, '0x0000000a'),
-            ('W', 0, '0x0000000b'),
+            command_factory('W', 0, '0x0000000a'),
+            command_factory('W', 0, '0x0000000b'),
         ],
         'optimized': [
             command_factory('W', 0, '0x0000000b'),
@@ -108,9 +52,9 @@ cases_cls = [
     },
     {
         'buff': [
-            ('E', 0, 3),
-            ('W', 4, '0x0000000a'),
-            ('E', 0, 5),
+            command_factory('E', 0, 3),
+            command_factory('W', 4, '0x0000000a'),
+            command_factory('E', 0, 5),
 
         ],
         'optimized': [
@@ -119,9 +63,9 @@ cases_cls = [
     },
     {
         'buff': [
-            ('W', 20, '0x0000000a'),
-            ('E', 10, 4),
-            ('E', 12, 3),
+            command_factory('W', 20, '0x0000000a'),
+            command_factory('E', 10, 4),
+            command_factory('E', 12, 3),
 
         ],
         'optimized': [
@@ -131,11 +75,11 @@ cases_cls = [
     },
     {
         'buff': [
-            ('E', 1, 1),
-            ('E', 3, 1),
-            ('E', 5, 1),
-            ('E', 7, 1),
-            ('E', 9, 1),
+            command_factory('E', 1, 1),
+            command_factory('E', 3, 1),
+            command_factory('E', 5, 1),
+            command_factory('E', 7, 1),
+            command_factory('E', 9, 1),
         ],
         'optimized': [
             command_factory('E', 1, 1),
@@ -147,11 +91,11 @@ cases_cls = [
     },
     {
         'buff': [
-            ('W', 1, '1'),
-            ('W', 3, '1'),
-            ('W', 5, '1'),
-            ('W', 7, '1'),
-            ('W', 9, '1'),
+            command_factory('W', 1, '1'),
+            command_factory('W', 3, '1'),
+            command_factory('W', 5, '1'),
+            command_factory('W', 7, '1'),
+            command_factory('W', 9, '1'),
         ],
         'optimized': [
             command_factory('W', 1, '1'),
@@ -163,11 +107,11 @@ cases_cls = [
     },
     {
         'buff': [
-            ('W', 1, DEFAULT_VALUE),
-            ('W', 2, DEFAULT_VALUE),
-            ('W', 3, DEFAULT_VALUE),
-            ('W', 4, DEFAULT_VALUE),
-            ('W', 5, DEFAULT_VALUE),
+            command_factory('W', 1, DEFAULT_VALUE),
+            command_factory('W', 2, DEFAULT_VALUE),
+            command_factory('W', 3, DEFAULT_VALUE),
+            command_factory('W', 4, DEFAULT_VALUE),
+            command_factory('W', 5, DEFAULT_VALUE),
         ],
         'optimized': [
             command_factory('E', 1, 5),
@@ -176,14 +120,14 @@ cases_cls = [
 ]
 
 
-def make_temp_cache(cmd) -> dict:
+def make_temp_cache(cmds) -> dict:
     cache_temp = {i: None for i in range(캐시사이즈)}
-    for action_type, addr, c in cmd:
-        if action_type == 'E':
-            for i in range(addr, addr + c):
+    for cmd in cmds:
+        if cmd.mode == 'E':
+            for i in range(cmd.lba, cmd.lba + cmd.size):
                 cache_temp[i] = DEFAULT_VALUE
-        if action_type == 'W':
-            cache_temp[addr] = c
+        if cmd.mode == 'W':
+            cache_temp[cmd.lba] = cmd.value
     return cache_temp
 
 
@@ -196,7 +140,7 @@ def test_controller_buffer_optimize_method_is_exist(controller):
     assert hasattr(controller, 'buffer_optimize')
 
 
-@pytest.mark.parametrize("buff_cmd, optimized_cmd", [(case['buff'], case['optimized']) for case in cases])
+@pytest.mark.parametrize("buff_cmd, optimized_cmd", [(case['buff'], case['optimized']) for case in cases_cls])
 def test_controller_pick_smaller_commands(controller, buff_cmd, optimized_cmd):
     picked = buff_cmd
     if len(optimized_cmd) < len(buff_cmd):
