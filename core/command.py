7189269b7773dadbaa7088cacd032ab0f7d4c053
junsik <junsik@ma.il>
2025-08-07 15:33:09 +0900

    [feature] Command 검증 위해 __eq__메서드 추가

--- core/command.py
+++ core/command.py
@@ -9,6 +9,9 @@ class Command(ABC):
     value: str = None  # 기본값 None
     def __init__(self, *args):
         ...
+
+
+
 class ReadCommand(Command):
     def __init__(self, *args):
         self.command = args[0]
@@ -19,12 +22,23 @@ class WriteCommand(Command):
         self.lba = args[1]
         self.value = args[2]
         self.size = 1
+
+    def __eq__(self, other):
+        if not isinstance(other, WriteCommand):
+            return False
+        return self.command == other.command and self.lba == other.lba and self.size == other.size and self.value == other.value
+
+
 class EraseCommand(Command):
     def __init__(self, *args):
         self.command = args[0]
         self.lba = args[1]
         self.value = DEFAULT_VALUE
         self.size = args[2]
+    def __eq__(self, other):
+        if not isinstance(other, EraseCommand):
+            return False
+        return self.command == other.command and self.lba == other.lba and self.size == other.size and self.value == other.value
 
 class FlushCommand(Command):
     def __init__(self, *args):
