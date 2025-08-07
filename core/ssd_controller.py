f56b817a1f623629575d48c978806741b7ce3c47
junsik <junsik@ma.il>
2025-08-07 13:59:03 +0900

    [feature] buffer_optimize 함수 에 temp_cache 주입하여 신규 cmd 생성 완료

--- core/ssd_controller.py
+++ core/ssd_controller.py
@@ -2,34 +2,20 @@ import json, os
 import argparse
 
 from core.validator import ControllerValidator
-from core.command import Command, command_factory, DEFAULT_VALUE
 
 ERROR = 'ERROR'
 PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 SSD_NAND_PATH = os.path.join(PROJECT_ROOT, 'ssd_nand.txt')
 SSD_OUTPUT_PATH = os.path.join(PROJECT_ROOT, 'ssd_output.txt')
+DEFAULT_VALUE = '0x00000000'
 
 
 class SSDController:
     def __init__(self):
         self.validator = ControllerValidator()
-        self.buffer = None
-        # self.update_cache(self.buffer.get())
-        self.update_cache(None)
-
-    def update_cache(self, buffer):
-        self.cache = {}
-        if buffer is None:
-            return self.cache
-        for command in buffer:
-            for i in range(command.size):
-                self.cache[command.lba + i] = command.value
-        return self.cache
-
     def ssd_nand_init(self):
         with open(SSD_NAND_PATH, 'w') as f:
             json.dump({}, f)
-
     def read(self, addr: int) -> None:
         try:
             if self.validator.is_lba_bad(addr):
@@ -58,35 +44,13 @@ class SSDController:
             self.output(ERROR)
             return False
 
-    def erase(self, addr: int, size: int) -> bool:
-        try:
-            if self.validator.is_lba_bad(addr) or self.validator.is_size_bad(size):
-                self.output(ERROR)
-                return False
-
-            self.update_nand_txt(addr, DEFAULT_VALUE, size=size)
-            return True
-        except:
-            self.output(ERROR)
-            return False
-
-    def flush(self) -> bool:
-        try:
-            buffer = self.buffer.get()
-            for command in buffer:
-                self.run(command)
-            self.buffer.truncate()
-        except:
-            self.output(ERROR)
-            return False
-    def update_nand_txt(self, addr, val, size=1) -> None:
+    def update_nand_txt(self, addr, val) -> None:
         if not os.path.exists(SSD_NAND_PATH):
             self.ssd_nand_init()
         try:
             with open(SSD_NAND_PATH, "r") as f:
                 memory = json.load(f)
-                for i in range(size):
-                    memory[str(addr + i)] = val.lower()
+                memory[str(addr)] = val.lower()
         except json.decoder.JSONDecodeError:
             self.ssd_nand_init()
         with open(SSD_NAND_PATH, "w") as f:
@@ -96,25 +60,22 @@ class SSDController:
     def output(self, data):
         with open(SSD_OUTPUT_PATH, "w", encoding="utf-8") as f:
             f.write(data)
-    def execute(self, command:Command):
-        if command.command == "R":
-            self.read(command.lba)
-        elif command.command == "W":
-            self.write(command.lba, command.value)
-        elif command.command  == "E":
-            self.erase(command.lba, command.size)
-        elif command.command  == "F":
-            self.flush()
 
 
 def main():
     parser = argparse.ArgumentParser(description="SSD Controller")
-    parser.add_argument("mode", choices=["R", "W", "E", "F"], help="모드 선택: R(Read) 또는 W(Write)")
-    parser.add_argument("lba", type=int, nargs="?", help="LBA 주소 (0~99)")
-    parser.add_argument("param", nargs="?", help="write일 경우 저장할 값 (0x로 시작하는 8자리 HEX), erase일 경우 삭제할 size")
+    parser.add_argument("mode", choices=["R", "W"], help="모드 선택: R(Read) 또는 W(Write)")
+    parser.add_argument("address", type=int, help="LBA 주소 (0~99)")
+    parser.add_argument("value", nargs="?", help="저장할 값 (0x로 시작하는 8자리 HEX, Write일 때만 사용)")
 
+    args = parser.parse_args()
     controller = SSDController()
-    controller.execute(command_factory(parser.parse_args()))
+    # 동작 선택
+    if args.mode == "R":
+        controller.read(args.address)
+    elif args.mode == "W":
+        controller.write(args.address, args.value)
+
 
 if __name__ == "__main__":
     main()
---
+++ tests/test_controller_buffer_optimize.py
@@ -0,0 +1,91 @@
+import pytest
+from core.ssd_controller import SSDController
+
+cases = [
+    {
+        'buff': [
+            ('E', 0, 10),
+            ('W', 5, '1')
+        ],
+        'optimized': [
+            ('E', 0, 5),
+            ('W', 5, '1'),
+            ('E', 6, 4)
+        ],
+    },
+    {
+        'buff': [
+            ('E', 0, 10),
+            ('W', 5, '1'),
+            ('W', 7, '1'),
+        ],
+        'optimized': [
+            ('E', 0, 5),
+            ('W', 5, '1'),
+            ('E', 6, 1),
+            ('W', 7, '1'),
+            ('E', 8, 2),
+        ],
+    },
+    {
+        'buff': [
+            ('E', 0, 4),
+            ('E', 4, 5)
+        ],
+        'optimized': [
+            ('E', 0, 9),
+        ],
+    },
+    {
+        'buff': [
+            ('W', 0, 'a'),
+            ('W', 0, 'b'),
+        ],
+        'optimized': [
+            ('W', 0, 'b'),
+        ],
+    },
+    {
+        'buff': [
+            ('E', 0, 3),
+            ('W', 4, 'a'),
+            ('E', 0, 5),
+
+        ],
+        'optimized': [
+            ('E', 0, 5),
+        ],
+    },
+]
+
+디폴트값 = '0x00000000'
+캐시사이즈 = 100
+
+
+def make_temp_cache(cmd) -> dict:
+    cache_temp = {i: None for i in range(캐시사이즈)}
+    for action_type, addr, c in cmd:
+        if action_type == 'E':
+            for i in range(addr, addr + c):
+                cache_temp[i] = 디폴트값
+        if action_type == 'W':
+            cache_temp[addr] = c
+    return cache_temp
+
+
+@pytest.fixture
+def controller():
+    return SSDController()
+
+
+def test_controller_buffer_optimize_method_is_exist(controller):
+    assert hasattr(controller, 'buffer_optimize')
+
+
+@pytest.mark.parametrize("buffcmd, optimized_cmd", [(i['buff'], i['optimized']) for i in cases])
+def test_controller_buffer_try(controller, buffcmd, optimized_cmd):
+    temp_cache = make_temp_cache(buffcmd)
+    controller.buffer_optimize(temp_cache, buffcmd)
+    assert controller.buffer_optimize(temp_cache, buffcmd) ==optimized_cmd
+
+
