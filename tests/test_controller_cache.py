6b62cdcd7d0e00ccfa6069ecab1498298642f38e
si <psiluv@gmail.com>
2025-08-07 14:44:29 +0900

    [feature]add command factory

--- tests/test_controller_cache.py
+++ tests/test_controller_cache.py
@@ -1,29 +1,33 @@
 import pytest
 from pytest_mock import mocker
 from core.ssd_controller import SSDController
+from core.command import Command, DEFAULT_VALUE
 
 
 @pytest.fixture
 def successs_buffer():
-    return [
-        {"command": "W", "lba": 0, "size": None, "value": "0xAAAABBBB"},
-        {"command": "W", "lba": 1, "size": None, "value": "0xCCCCDDDD"},
-        {"command": "E", "lba": 2, "size": 10, "value": None},
-        {"command": "W", "lba": 3, "size": None, "value": "0x12345678"},
-        {"command": "E", "lba": 4, "size": 1, "value": None},
-        {"command": "W", "lba": 5, "size": None, "value": "0xDEADBEEF"},
-    ],{0: '0xAAAABBBB',
-          1: '0xCCCCDDDD',
-          2: '0x00000000',
-          3: '0x12345678',
-          4: '0x00000000',
-          5: '0xDEADBEEF',
-          6: '0x00000000',
-          7: '0x00000000',
-          8: '0x00000000',
-          9: '0x00000000',
-          10: '0x00000000',
-          11: '0x00000000'}
+    buffer = [
+        Command("W", 0, "0xAAAABBBB"),
+        Command("W", 1, "0xCCCCDDDD"),
+        Command("E", 2, 10),
+        Command("W", 3, "0x12345678"),
+        Command("E", 4, 1),
+        Command("W", 5, "0xDEADBEEF")
+    ]
+    cache = {0: '0xAAAABBBB',
+             1: '0xCCCCDDDD',
+             2: '0x00000000',
+             3: '0x12345678',
+             4: '0x00000000',
+             5: '0xDEADBEEF',
+             6: '0x00000000',
+             7: '0x00000000',
+             8: '0x00000000',
+             9: '0x00000000',
+             10: '0x00000000',
+             11: '0x00000000'}
+    return buffer, cache
+
 
 def test_update_cache_success(successs_buffer):
     buffer, expected_cache = successs_buffer
