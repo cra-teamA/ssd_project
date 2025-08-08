1565db4fa10b0e9460da89c892976bf824be3070
si <psiluv@gmail.com>
2025-08-08 12:53:37 +0900

    [fix]change read output format to lower case

--- core/command.py
+++ core/command.py
@@ -2,22 +2,28 @@ from abc import ABC
 
 DEFAULT_VALUE = '0x00000000'
 
+
 class Command(ABC):
     mode: str
     lba: int
     size: int
     value: str = None  # 기본값 None
+
     def __init__(self, *args):
         ...
+
+
 class ReadCommand(Command):
     def __init__(self, *args):
         self.mode = args[0]
         self.lba = args[1]
+
+
 class WriteCommand(Command):
     def __init__(self, *args):
         self.mode = args[0]
         self.lba = args[1]
-        self.value = args[2]
+        self.value = args[2].lower()
         self.size = 1
 
     def __eq__(self, other):
@@ -32,16 +38,19 @@ class EraseCommand(Command):
         self.lba = args[1]
         self.value = DEFAULT_VALUE
         self.size = int(args[2])
+
     def __eq__(self, other):
         if not isinstance(other, EraseCommand):
             return False
         return self.mode == other.mode and self.lba == other.lba and self.size == other.size and self.value == other.value
 
+
 class FlushCommand(Command):
     def __init__(self, *args):
         self.mode = args[0]
-def command_factory(mode, lba, param):
 
+
+def command_factory(mode, lba, param):
     if mode == 'F':
         return FlushCommand(mode)
     if mode == 'R':
