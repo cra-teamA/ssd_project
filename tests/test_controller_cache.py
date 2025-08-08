5b3211ffbacce787ab0c34a222b7fd669318c7ad
soiscow <thgus_so@naver.com>
2025-08-08 13:43:26 +0900

    [refactor] test code에 대문자 적용

--- tests/test_controller_cache.py
+++ tests/test_controller_cache.py
@@ -7,19 +7,19 @@ from core.command import command_factory, DEFAULT_VALUE
 @pytest.fixture
 def successs_buffer():
     buffer = [
-        command_factory("W", 0, "0xAAAABBBB"),
-        command_factory("W", 1, "0xCCCCDDDD"),
+        command_factory("W", 0, "0xaaaabbbb"),
+        command_factory("W", 1, "0xccccdddd"),
         command_factory("E", 2, 10),
         command_factory("W", 3, "0x12345678"),
         command_factory("E", 4, 1),
-        command_factory("W", 5, "0xDEADBEEF")
+        command_factory("W", 5, "0xdeadbeef")
     ]
-    cache = {0: '0xAAAABBBB',
-             1: '0xCCCCDDDD',
+    cache = {0: '0xaaaabbbb',
+             1: '0xccccdddd',
              2: '0x00000000',
              3: '0x12345678',
              4: '0x00000000',
-             5: '0xDEADBEEF',
+             5: '0xdeadbeef',
              6: '0x00000000',
              7: '0x00000000',
              8: '0x00000000',
